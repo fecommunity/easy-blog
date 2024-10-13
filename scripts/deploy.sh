4ec3e3d7c04e1712597c5f62142cb624f1d68428
m0_37981569 <admin@gaoredu.com>
2024-10-13 12:24:51 +0800

    chore: update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,16 @@
-#! /bin/bash
-
-cd easy-blog
+# 更新代码
 git checkout master
 git pull
 
+# 安装依赖&构建
 pnpm install
 pnpm run build
+
+# 启动进程
+pm2 delete @easy-blog/server
+pm2 delete @easy-blog/client
 pnpm run pm2
 
+# 开机启动
 pm2 startup
 pm2 save
