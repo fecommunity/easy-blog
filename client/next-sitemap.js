282052e6b4755743e20f8efcde532973d7b09ee4
m0_37981569 <admin@gaoredu.com>
2024-12-01 23:44:30 +0800

    refactor: update next-sitemap config

--- client/next-sitemap.js
+++ client/next-sitemap.js
@@ -3,4 +3,8 @@ const { config } = require('@reactpress/config');
 module.exports = {
   siteUrl: config.CLIENT_SITE_URL,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/', disallow: '/admin/' }],
+  },
+  exclude: ['/admin', '/admin/**'],
 };
